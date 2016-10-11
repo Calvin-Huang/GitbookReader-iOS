727a0afd9c779b5e7178c810c1eb8a3209306ada
Calvin <calvin.peak@capslock.tw>
2016-10-11 16:57:17 +0900

    Add user view model structure.

--- GitbookReader/ViewModels/UserViewModel.swift
+++ GitbookReader/ViewModels/UserViewModel.swift
@@ -9,5 +9,20 @@
 import RxSwift
 
 class UserViewModel {
-    let currentUser: Observable<User?> = Observable.just(nil)
+    let currentUser: Variable<User?> = Variable(nil)
+    
+    static let sharedInstance = UserViewModel()
+    
+    private init() {}
+    
+    func authenticateGitBook() {
+        guard let associatedDomain = (Bundle.main.object(forInfoDictionaryKey: "Associated Domain") as AnyObject?)?["Production"] as? String else {
+            return
+        }
+        UIApplication.shared.openURL(URL(string: "\(associatedDomain)/users/auth/gitbook")!)
+    }
+    
+    func fetchUser(token: String) {
+        
+    }
 }
