05c466c735a80b99e0c216bef05b00bfb597853d
Calvin <calvin.peak@capslock.tw>
2016-10-13 04:06:15 +0900

    In this app, user can do almost every things without sign in. Therefore we need to remind user that if he/she signed in, they can use more convenience functions.

--- GitbookReader/ViewControllers/BooksViewController.swift
+++ GitbookReader/ViewControllers/BooksViewController.swift
@@ -21,27 +21,92 @@ class BooksViewController: UIViewController {
         }
     }
     
+    var queuedAction: (() -> Void)?
+    
     private let disposeBag = DisposeBag()
-    private let userViewModel = UserViewModel.sharedInstance
+    
+    private lazy var actionSheet: UIAlertController = {
+        let actionSheet = UIAlertController(title: "You can add book by", message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(
+            UIAlertAction(title: "Enter remote URL", style: .default, handler: { [weak self] _ in
+                self?.present(AddBookViewController(), animated: true, completion: nil)
+            })
+        )
+        
+        actionSheet.addAction(
+            UIAlertAction(title: "Search in gitbook.com", style: .default, handler: { [weak self] _ in
+                self?.present(SearchBookViewController(), animated: true, completion: nil)
+            })
+        )
+        
+        actionSheet.addAction(
+            UIAlertAction(title: "Import from your stars", style: .default, handler: { [weak self] _ in
+                let userViewModel = UserViewModel.sharedInstance
+                if let user = userViewModel.currentUser.value {
+                    userViewModel.fetchStarrdBooks()
+                } else {
+                    let authenticateViewController = AuthenticateModalViewController(
+                        confirmed: { [weak self] in
+                            guard let viewController = $0 as? UIViewController else {
+                                return
+                            }
+                            
+                            self?.queuedAction = { self?.showImportBookFromStarsHintModal() }
+                            viewController.dismiss(animated: false)
+                        }
+                    )
+                    
+                    self?.present(authenticateViewController, animated: true, completion: nil)
+                }
+            })
+        )
+        
+        actionSheet.addAction(
+            UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        )
+        
+        return actionSheet
+    }()
+    private lazy var importBookFromStarsHintModal: BasicModalViewController = {
+        let modalViewController = BasicModalViewController(
+            message: "You can add books from your starred!",
+            confirmed: {
+                guard let viewController = $0 as? UIViewController else { return }
+                
+                UserViewModel.sharedInstance.fetchStarrdBooks()
+                
+                viewController.dismiss(animated: true, completion: nil)
+            }
+        )
+        
+        return modalViewController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureSearchBar()
         configureKeyboardDismissesOnScroll()
+        configureBooksTable()
         
         addBookButton.rx.tap
             .subscribe(onNext: { [weak self] _ in
-                if let _ = self?.userViewModel.currentUser.value {
+                if let _ = UserViewModel.sharedInstance.currentUser.value {
                     self?.showAddBookActionSheet()
                 } else {
-                    let authenticateViewController = AuthenticateModalViewController(confirmed: {
-                        guard let viewController = $0 as? UIViewController else {
-                            return
+                    let authenticateViewController = AuthenticateModalViewController(
+                        confirmed: { [weak self] in
+                            guard let viewController = $0 as? UIViewController else {
+                                return
+                            }
+                            
+                            self?.queuedAction = { self?.showAddBookActionSheet() }
+                            viewController.dismiss(animated: false)
+                        },
+                        canceled: { [weak self] in
+                            self?.showAddBookActionSheet()
                         }
-                        
-                        viewController.dismiss(animated: false)
-                    })
+                    )
                     
                     self?.present(authenticateViewController, animated: true, completion: nil)
                 }
@@ -67,12 +132,24 @@ class BooksViewController: UIViewController {
     }
     
     func showAddBookActionSheet() {
-        
+        self.present(self.actionSheet, animated: true, completion: nil)
+    }
+    
+    func showImportBookFromStarsHintModal() {
+        self.present(self.importBookFromStarsHintModal, animated: true, completion: nil)
     }
 
     // MARK: Private Methods
     private func configureBooksTable() {
+        booksTable.register(UINib(nibName: "\(BookCell.self)", bundle: nil), forCellReuseIdentifier: "\(BookCell.self)")
+        booksTable.rowHeight = 74
         
+        UserViewModel.sharedInstance.ownedBooks
+            .asObservable()
+            .bindTo(booksTable.rx.items(cellIdentifier: "\(BookCell.self)", cellType: BookCell.self)) { (row, viewModel, cell) in
+                cell.viewModel = viewModel
+            }
+            .addDisposableTo(disposeBag)
     }
     
     private func configureSearchBar() {
